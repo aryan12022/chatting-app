e567d36ccecf50bc0f6c161a206726d8e783e551
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-09-04 01:50:20 +0530

    dev mode

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from "path";
+// import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -22,7 +22,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -33,11 +33,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 server.listen(PORT, async() => {
 	await connectToMongoDB();
