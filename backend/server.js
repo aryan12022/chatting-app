0e29d2a8539fc7d9e973a268e2de0cef1796c8a4
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-09-09 11:12:38 +0530

    chore: updated cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import cors from "cors";
 const FRONTEND_URL = process.env.FRONTEND_URL || "https://chatting-app-livid-eight.vercel.app/";
 
 app.use(cors({
-  origin: [FRONTEND_URL, "http://localhost:3000"],
+  origin: ["*"],
   credentials: true,
 }));
 
